37fe2c6d56820603d8ac8ec969a4883f3b816191
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-03-31 11:14:27 -0700

    Updated duralock side a to include reset function.

--- duralock_side_a/duralock_side_a.ino
+++ duralock_side_a/duralock_side_a.ino
@@ -1,18 +1,19 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
+void(* resetFunc) (void) = 0;//reset function is built-in
+
 const int boton = 9;
+
 int valor = 0;
 
 // Enter a MAC address for your controller below.
-// Newer Ethernet shields have a MAC address printed on a sticker on the shield
-byte mac[] = { 0xDE, 0xAD, 0xBD, 0xEF, 0xFE, 0xDF }; //if it doesnt have a sticker just make it up as long no device on your network has this same mac you should be fine
+byte mac[] = { 0xDE, 0xAD, 0xDB, 0xEF, 0xFE, 0xFD }; //if it doesnt have a sticker just make it up as long no device on your network has this same mac you should be fine
 
-// if you don't want to use DNS (and reduce your sketch size)
-// use the numeric IP instead of the name for the server:
-//IPAddress server(174,136,53,235);  // mi servidor en internet
-IPAddress server(192,168,1,141);  // mi servidor en internet
-//char server[] = "http://mxmtsvrandon1";    
+
+IPAddress server(192,168,1,141);  // Numeric address
+//char server[] = "http://mxmtsvrandon1"; //you can use DNS too.    
+
 
 // Set the static IP address to use if the DHCP fails to assign
 IPAddress ip(192, 168, 8, 54);
@@ -23,6 +24,7 @@ IPAddress myDns(192, 168, 0, 1);
 // that you want to connect to (port 80 is default for HTTP):
 EthernetClient client;
 
+
 // Variables to measure the speed
 unsigned long beginMicros, endMicros;
 unsigned long byteCount = 0;
@@ -31,43 +33,47 @@ bool printWebData = true;  // set to false for better speed measurement
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(boton, OUTPUT);
+    pinMode(boton, OUTPUT);
     pinMode(LED_BUILTIN, INPUT);
 
     // You can use Ethernet.init(pin) to configure the CS pin
     Ethernet.init(10);  // Most Arduino shields
-    //Ethernet.init(5);   // MKR ETH shield
-    //Ethernet.init(0);   // Teensy 2.0
-    //Ethernet.init(20);  // Teensy++ 2.0
-    //Ethernet.init(15);  // ESP8266 with Adafruit Featherwing Ethernet
-    //Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
-
-    // Open serial communications and wait for port to open:
+   
+    
     Serial.begin(9600);
-    while (!Serial) {
+    while (!Serial) 
+    {
         ; // wait for serial port to connect. Needed for native USB port only
     }
 
     // start the Ethernet connection:
     Serial.println("Initialize Ethernet with DHCP:");
-    if (Ethernet.begin(mac) == 0) {
+
+    
+    if (Ethernet.begin(mac) == 0) 
+    {
         Serial.println("Failed to configure Ethernet using DHCP");
         // Check for Ethernet hardware present
-        if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+        if (Ethernet.hardwareStatus() == EthernetNoHardware) 
+        {
             Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
             while (true) {
                 delay(1); // do nothing, no point running without Ethernet hardware
             }
         }
-        if (Ethernet.linkStatus() == LinkOFF) {
+        if (Ethernet.linkStatus() == LinkOFF) 
+        {
             Serial.println("Ethernet cable is not connected.");
         }
         // try to congifure using IP address instead of DHCP:
         Ethernet.begin(mac, ip, myDns);
-    } else {
+    } 
+    else 
+    {
         Serial.print("  DHCP assigned IP ");
         Serial.println(Ethernet.localIP());
     }
+    
     // give the Ethernet shield a second to initialize:
     delay(1000);
     Serial.print("connecting to ");
@@ -78,38 +84,42 @@ void setup() {
 
 void loop() {
  valor = digitalRead(boton);
-    if(valor == HIGH) {
+    if(valor == HIGH) 
+    {
         digitalWrite(LED_BUILTIN, HIGH);
         delay(2000);
   
         // if you get a connection, report back via serial:
-        if (client.connect(server, 80)== 1) {
+        if (client.connect(server, 80)== 1) 
+        {
             Serial.print("connected to ");
             Serial.println(client.remoteIP());
             // Make a HTTP request:
-            client.println("GET /production-tracker-medcomp/Api/v1/insert.php?site=1038&value=1 HTTP/1.1");
+            client.println("GET /production-tracker-medcomp/Api/v1/insert.php?site=1037&value=1 HTTP/1.1");
             client.println("Host: 192.168.1.141");
             client.println("Connection: close");
             client.println();
 
-            /*
-            client.println("/arduino/arduino.php?number=1 HTTP/1.1");
-            client.println("Host: http://mxmtsvrandon1");
-            client.println("Connection: close");
-            client.println();
-            */
+       
 
 
-
-        } else {
+        } 
+        else 
+        {
             // if you didn't get a connection to the server:
             Serial.println("connection failed");
+            delay(1000);
+            Serial.println("Trying to reconnect...");
+            delay(1000);
+
+            resetFunc();
         }
         beginMicros = micros();
         // if there are incoming bytes available
         // from the server, read them and print them:
         int len = client.available();
-        if (len > 0) {
+        if (len > 0) 
+        {
             byte buffer[80];
             if (len > 80) len = 80;
             client.read(buffer, len);
@@ -120,7 +130,8 @@ void loop() {
         }
 
         // if the server's disconnected, stop the client:
-        if (!client.connected()) {
+        if (!client.connected()) 
+        {
             endMicros = micros();
             Serial.println();
             Serial.println("disconnecting.");
@@ -139,7 +150,8 @@ void loop() {
             setup();
 
             // do nothing forevermore:
-            while (true) {
+            while (true) 
+            {
                 delay(1000);
             }
         }
